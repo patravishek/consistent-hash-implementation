1eb235c89314ab6753ce6170f925ba520f4439ed
Avishek Patra <avishek.patra@outlook.com>
2020-05-29 09:31:27 +0530

    Adding changes

--- hashing.js
+++ hashing.js
@@ -2,21 +2,27 @@
 
 const ConsistentHashing = require('consistent-hashing');
 const cons = new ConsistentHashing(["node1", "node2", "node3"]);
+const lib = require("./display");
 
-const nodes = {};
 const chars = [
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'
 ];
 
-chars.forEach(function(c) {
-  const node = cons.getNode(c);
+let nodes1 = lib.generateServerNames(chars,cons)
 
-  if (nodes[node]) {
-    nodes[node].push(c);
-  } else {
-    nodes[node] = [];
-    nodes[node].push(c);
-  }
-});
+console.log("Node arrangement at the initial level"); 
+console.log(nodes1);
 
-console.log(nodes);
+
+cons.addNode("node4");
+
+let nodes2 = lib.generateServerNames(chars, cons);
+
+console.log("Node arrangement after adding a new node");
+console.log(nodes2);
+
+cons.removeNode("node2");
+
+console.log("Node arrangement after removing node 2");
+let nodes3 = lib.generateServerNames(chars, cons);
+console.log(nodes3);
